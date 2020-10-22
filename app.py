26f5207be2bec6ea168c733c1d29fce5008e314f
Kelvin <kwardeu@gmail.com>
2020-10-22 13:39:36 +0000

    FEAT: Add MongoDB and enviromental variable

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+# Create constants for URI login data / database / collection in database
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "task_manager"
+COLLECTION = "tasks"
+
+app.config["MONGO_DBNAME"] = DATABASE
+app.config["MONGO_URI"] = MONGO_URI
+
+mongo = PyMongo(app) #Connect to mongoDB through flask_pymongo > PyMongo(this_flask_app)
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
