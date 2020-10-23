353022c5d6e23cda11c1bb14b5db4a8610b76e94
Kelvin <kwardeu@gmail.com>
2020-10-23 17:17:54 +0000

    FEAT: Add compteted tasks system and 'redo/delete' button

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mongo = PyMongo(app) #Connect to mongoDB through flask_pymongo > PyMongo(this_fl
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", tasks=list(mongo.db.tasks.find())) # Turned task itterator to list so I can loop through it more than once
 
 @app.route('/add_task')
 def add_task():
@@ -37,6 +37,37 @@ def edit_task(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task, categories=all_categories)
+
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)},
+        {
+            'task_name': request.form.get('task_name'),
+            'category_name': request.form.get('category_name'),
+            'task_description': request.form.get('task_description'),
+            'due_date': request.form.get('due_date'),
+            'is_urgent': request.form.get('is_urgent'),
+            'task_completed': request.form.get('task_completed')
+        })
+    return redirect(url_for('get_tasks'))
+
+@app.route('/done_task/<task_id>')
+def done_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)}, {"$set": {"task_completed": "on"}})
+    return redirect(url_for('get_tasks'))
+
+@app.route('/redo_task/<task_id>')
+def redo_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)}, {"$set": {"task_completed": ""}})
+    return redirect(url_for('get_tasks'))
+
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
